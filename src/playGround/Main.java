19efee7be1d8c41d8dfa3807bba72f7825c174b6
AnthonyGAlmanza <antman_2012@yahoo.com>
2021-01-27 17:50:59 -0600

    initial commit

---
+++ src/playGround/Main.java
@@ -0,0 +1,46 @@
+package playGround;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String package; 
+		Integer void; // These two declarations will not work as package and void are reserved words in Java
+		
+		for(int i = 1;  ;) {
+	}
+
+		
+		if(true) {
+			int number = 5;
+		}
+		
+		System.out.println(number); // Cannot print this variable as it is outside of scope and only exists in the if statement
+		
+		
+		
+		
+		for(int i = 1; i < 10; i--) {
+			System.out.println(i); // No error is provided but this is an infinite loop and will run forever
+			
+		}
+		
+		
+		
+		int numberOne;
+		
+		System.out.println(numberOne); // Cannot be printed as variable numberOne has not been initialized
+		
+		
+		
+		String sentenceOne = 'Hello there!'; // Invalid character constant. Strings require double quotes ""
+		
+
+		
+	}	
+		
+}
+	
+	
+	
+	
